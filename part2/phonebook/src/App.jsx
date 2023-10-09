8d864b22d35f7b9d7b1a656405dff3137d4b067a
Alex <thanhduonghd114@gmail.com>
2023-10-10 01:37:57 +0300

    bugs in step1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,20 +5,39 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  console.log(persons.name)
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit="addName">
         <div>
-          name: <input />
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+        {persons.map((person) => (
+          <p>
+            {person.name}
+          </p>
+        ))}
     </div>
   )
 }
