d8ff5a6d3a691254fd3217c845e34b72ee07486c
Alex <thanhduonghd114@gmail.com>
2023-11-03 16:31:33 +0200

    fix bugs

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -99,6 +99,7 @@ app.post('/api/persons', (req, res) => {
   phonebooks = phonebooks.concat(phone)
   res.json(phonebooks)
 })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
