960a5cec900eeb7f0731650bb8d8cce6126ff271
Alex <thanhduonghd114@gmail.com>
2023-11-26 17:04:46 +0200

    done blog list step2

---
+++ part4/blog-list/server/controllers/blog-list.js
@@ -0,0 +1,29 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog-list')
+
+blogsRouter.get('/', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  
+  blogsRouter.post('/', (request, response) => {
+    const blogData = request.body
+
+    const blog = new Blog({
+        title: blogData.title,
+        author: blogData.author,
+        url: blogData.url,
+        likes: blogData.likes
+    })
+
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+  })
+
+  module.exports = blogsRouter
