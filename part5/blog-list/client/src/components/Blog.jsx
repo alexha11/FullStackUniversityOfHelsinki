bd6a9c7423032c16bd04a564334395547aba0e8b
Alex <thanhduonghd114@gmail.com>
2023-12-31 21:41:55 +0200

    test

--- part5/blog-list/client/src/components/Blog.jsx
+++ part5/blog-list/client/src/components/Blog.jsx
@@ -1,7 +1,9 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({ blog }) => {
+  return (
+    <div>
+      {blog.title} {blog.author}
+    </div>  
+  )
+}
 
 export default Blog
